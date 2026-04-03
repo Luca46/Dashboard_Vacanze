43012a058a9db2bcd915ff252a192f825bd6f277
Luca Puglisi <62299593+Luca46@users.noreply.github.com>
2026-04-03 18:32:04 +0200

    Add files via upload

---
+++ Dashboard_vacanze.py
@@ -0,0 +1,50 @@
+
+#%pip install gspread
+#%pip install streamlit
+import gspread
+import pandas as pd
+import streamlit as st
+st.set_page_config(layout="wide")
+
+service_account=gspread.service_account(filename=r'C:\Users\puglisil\OneDrive - UPMC\Documents\File\Personale\Tutto_Python_fatto_da_me\Esercizi_Progetti_Programmazione_Pacchetti\Progetto_Vacanza\chiave.json')
+nome_foglio=service_account.open('Vacanze Zafferana Etnea 6-9 Agosto  2026  (Risposte)').sheet1
+dizionario=nome_foglio.get_all_records()
+df=pd.DataFrame(dizionario)
+groupby_stanze=df.groupby('Tipologia Stanza').agg(Tipologia_Stanza= ('Tipologia Stanza','count'))
+count_stanze=df['Tipologia Stanza'].value_counts()
+
+
+iscritti_adulti1=len(df['Cognome Nome Adulto 1'])
+iscritti_adulti2=len(df[df['Cognome Nome Adulto 2']!='']['Cognome Nome Adulto 2'])
+iscritti_adulti3=len(df[df['Cognome Nome Adulto 3']!='']['Cognome Nome Adulto 3'])
+iscritti_adulti4=len(df[df['Cognome Nome Adulto 4']!='']['Cognome Nome Adulto 4'])
+iscritti_adulti5=len(df[df['Cognome Nome Adulto 5']!='']['Cognome Nome Adulto 5'])
+
+iscritti_bambini_1=len(df[df['Cognome Nome Ragazzo Bambino 1']!='']['Cognome Nome Ragazzo Bambino 1'])
+iscritti_bambini_2=len(df[df['Cognome Nome Ragazzo Bambino 2']!='']['Cognome Nome Ragazzo Bambino 2'])
+iscritti_bambini_3=len(df[df['Cognome Nome Ragazzo Bambino 3']!='']['Cognome Nome Ragazzo Bambino 3'])
+iscritti_bambini_4=len(df[df['Cognome Nome Ragazzo Bambino 4']!='']['Cognome Nome Ragazzo Bambino 4'])
+iscritti_bambini_5=len(df[df['Cognome Nome Ragazzo Bambino 5']!='']['Cognome Nome Ragazzo Bambino 5'])
+iscritti_adulti= iscritti_adulti1+iscritti_adulti2+iscritti_adulti3+iscritti_adulti4+iscritti_adulti5
+iscritti_ragazzi_bambini=iscritti_bambini_1+iscritti_bambini_2+iscritti_bambini_3+iscritti_bambini_4+iscritti_bambini_5
+iscritti_totali=iscritti_adulti+iscritti_ragazzi_bambini
+
+st.title('Dashboard vacanze Zafferana Etnea')
+
+iscritti_totale_col, iscritti_adulti_col, iscritti_bambini_col = st.columns(3)
+
+with iscritti_totale_col:
+    st.metric('Totale iscritti', iscritti_totali) # Usa "st", non il nome della colonna!
+
+with iscritti_adulti_col:
+    st.metric('Adulti iscritti', iscritti_adulti)
+
+with iscritti_bambini_col:
+    st.metric('Ragazzi bambini iscritti', iscritti_ragazzi_bambini)
+
+
+st.bar_chart(count_stanze)
+
+st.dataframe(df)
+
+st.balloons()
